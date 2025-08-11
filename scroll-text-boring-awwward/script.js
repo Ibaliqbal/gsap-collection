f8decbb4f7f9d3fca65bd235d01fc11dcf33122b
Ibaliqbal <iqbalmuthahhary@gmail.com>
2025-08-11 17:23:25 +0700

    done scroll text boring awwward

--- scroll-text-boring-awwward/script.js
+++ scroll-text-boring-awwward/script.js
@@ -9,4 +9,51 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   gsap.ticker.lagSmoothing(0);
+
+  const scrolling = document.querySelector(".scrolling");
+  const firstTexts = document.querySelectorAll(".text-wrap h1:first-of-type");
+  const lastTexts = document.querySelectorAll(".text-wrap h1:last-of-type");
+  const yPercent = 120;
+  const divider = document.querySelector(".divider");
+
+  ScrollTrigger.create({
+    trigger: scrolling,
+    start: "clamp(top top)",
+    end: "+=250%",
+    pin: true,
+    // markers: true,
+    scrub: true,
+    onUpdate: (self) => {
+      const progress = self.progress * 10;
+
+      if (progress <= 7) {
+        const target = 7;
+        const progressText = Math.round((progress / target) * yPercent);
+        gsap.set(lastTexts, {
+          y: `${yPercent - progressText}%`,
+          stagger: 0,
+        });
+        gsap.set(firstTexts, {
+          y: `-${progressText}%`,
+          stagger: 0,
+        });
+      }
+
+      if (progress >= 7 && progress <= 9) {
+        const dividerProgress = progress - 7;
+        console.log(dividerProgress);
+        gsap.set(divider, {
+          scaleX: dividerProgress,
+        });
+      } else if (progress < 7) {
+        gsap.set(divider, {
+          scaleX: 0,
+        });
+      } else if (progress > 9) {
+        gsap.set(divider, {
+          scaleX: 1,
+        });
+      }
+    },
+  });
 });
