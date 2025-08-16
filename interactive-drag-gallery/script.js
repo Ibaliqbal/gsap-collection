658473d98b43d6f4cea49fa9f46e29369a403032
Ibaliqbal <iqbalmuthahhary@gmail.com>
2025-08-17 01:20:11 +0700

    recreate similar interactive drag

---
+++ interactive-drag-gallery/script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(Observer, InertiaPlugin);
+  const gallery = document.querySelector(".wrapper");
+  const container = document.querySelector(".container");
+
+  const startX = (window.innerWidth - gallery.offsetWidth) / 2,
+    startY = (window.innerHeight - gallery.offsetHeight) / 2;
+
+  gsap.set(gallery, {
+    x: startX,
+    y: startY,
+  });
+
+  const yQuick = gsap.quickTo(gallery, "y", {
+      ease: "sine",
+      duration: 1,
+    }),
+    xQuick = gsap.quickTo(gallery, "x", {
+      ease: "sine",
+      duration: 1,
+    });
+
+  const maxX = -(gallery.offsetWidth - window.innerWidth);
+  const maxY = -(gallery.offsetHeight - window.innerHeight);
+
+  let xTrack = startX,
+    yTrack = startY;
+
+  Observer.create({
+    type: "pointer,touch",
+    onChange: (self) => {
+      xTrack += self.deltaX * 2.5;
+      yTrack += self.deltaY * 2.5;
+
+      xTrack = gsap.utils.clamp(maxX, 0, xTrack);
+      yTrack = gsap.utils.clamp(maxY, 0, yTrack);
+
+      yQuick(yTrack);
+      xQuick(xTrack);
+    },
+    onPress: () => {
+      container.style.cursor = "grabbing";
+      container.classList.add("active");
+    },
+    onRelease: () => {
+      container.style.cursor = "grab";
+      container.classList.remove("active");
+    },
+    tolerance: 10,
+  });
+});
